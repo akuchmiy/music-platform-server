dc20b00107dff9c36b94dbdf541c2e7e49198ede
Anton Kuchmii <a.kuchmiy19@gmail.com>
2021-12-15 17:05:37 +0200

    static content

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,21 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { configService } from './config/configService'
 import { TokenModule } from './modules/auth/modules/token/token.module'
 import { MailModule } from './modules/mail/mail.module'
-import { FileModule } from './modules/file/file.module';
-import { TrackModule } from './modules/track/track.module';
-import { BandModule } from './modules/band/band.module';
-import { AlbumModule } from './modules/album/album.module';
+import { FileModule } from './modules/file/file.module'
+import { TrackModule } from './modules/track/track.module'
+import { BandModule } from './modules/band/band.module'
+import { AlbumModule } from './modules/album/album.module'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path'
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'static'),
+    }),
     AuthModule,
     UserModule,
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     TokenModule,
     MailModule,
     FileModule,
